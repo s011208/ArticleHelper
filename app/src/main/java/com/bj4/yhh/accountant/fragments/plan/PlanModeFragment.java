f7d17b8437345c11193516a9397eaad58a2e41ca
Yen-Hsun_Huang <yen-hsun_huang@asus.com>
2015-08-20 19:19:58 +0800

    add TestActivity

--- app/src/main/java/com/bj4/yhh/accountant/fragments/plan/PlanModeFragment.java
+++ app/src/main/java/com/bj4/yhh/accountant/fragments/plan/PlanModeFragment.java
@@ -1,16 +1,19 @@
 package com.bj4.yhh.accountant.fragments.plan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ViewSwitcher;
 
 import com.bj4.yhh.accountant.R;
 import com.bj4.yhh.accountant.activity.MainActivity;
+import com.bj4.yhh.accountant.activity.TestActivity;
 import com.bj4.yhh.accountant.utils.BaseFragment;
 
 /**
@@ -33,6 +36,23 @@ public class PlanModeFragment extends BaseFragment implements View.OnClickListen
         ListView planList = (ListView) root.findViewById(R.id.plan_list);
         mPlanListAdapter = new PlanListAdapter(getActivity());
         planList.setAdapter(mPlanListAdapter);
+        planList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final Plan plan = mPlanListAdapter.getItem(i);
+                Intent intent = new Intent(getActivity(), TestActivity.class);
+                intent.putExtra(TestActivity.EXTRA_PLAN, plan.toString());
+                getActivity().startActivity(intent);
+            }
+        });
+        planList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final Plan plan = mPlanListAdapter.getItem(i);
+                // TODO delete dialog
+                return true;
+            }
+        });
 
         return root;
     }
@@ -63,6 +83,7 @@ public class PlanModeFragment extends BaseFragment implements View.OnClickListen
             if (DEBUG) {
                 Log.d(TAG, "remove_all_plans");
             }
+            // TODO delete dialog
             Plan.deleteAllPlans(getActivity());
             if (mPlanListAdapter != null) {
                 mPlanListAdapter.updateContent();
