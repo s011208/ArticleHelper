1b061e765138ca79192824c70f215383afca5fbb
Yen-Hsun, Huang <s011208@gmail.com>
2015-08-23 23:19:30 +0800

    add review mode activity

---
+++ app/src/main/java/com/bj4/yhh/accountant/activity/ReviewModeActivity.java
@@ -0,0 +1,17 @@
+package com.bj4.yhh.accountant.activity;
+
+import android.os.Bundle;
+
+import com.bj4.yhh.accountant.R;
+
+/**
+ * Created by yenhsunhuang on 15/8/23.
+ */
+public class ReviewModeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_mode);
+    }
+}
