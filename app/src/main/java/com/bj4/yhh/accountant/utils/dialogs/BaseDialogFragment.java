dfef266df3c0f4c02e0d5eae2b3128e6a215f9be
Yen-Hsun, Huang <s011208@gmail.com>
2015-08-27 23:55:57 +0800

    refactor

--- app/src/main/java/com/bj4/yhh/accountant/utils/dialogs/BaseDialogFragment.java
+++ app/src/main/java/com/bj4/yhh/accountant/utils/dialogs/BaseDialogFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
